b7d9d05fe821588779f43cff22602f30f184538a
CodinGitHub <gms2programador@gmail.com>
2022-04-13 06:15:05 -0500

    script for video

---
+++ main.js
@@ -0,0 +1,12 @@
+const days = {
+    0: 'Domingo',
+    1: 'Lunes',
+    2: 'Martes',
+    3: 'Miercoles',
+    4: 'Jueves',
+    5: 'Viernes',
+    6: 'Sábado'
+}
+
+let birthday = new Date(1984, 09, 28);
+console.log(days[birthday.getDay()]);
